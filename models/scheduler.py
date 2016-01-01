f5610e9876088672092989dd37aa1f574c515528
mukul hase <mukulhase@gmail.com>
2016-01-01 18:37:23 +0530

    Event timings changed

--- models/scheduler.py
+++ models/scheduler.py
@@ -4,8 +4,8 @@ def generate_mail():
     cc=[], bcc=[], reply_to=[], sender=None, encoding='utf-8',
      raw=True, headers={})'''
     pass
-
+'''
 from gluon.scheduler import Scheduler
 scheduler = Scheduler(db)
-
+'''
 # scheduler.queue_task(generate_mail, period=300, repeats=0)
